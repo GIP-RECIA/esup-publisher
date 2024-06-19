42f7540baf4485c2750d59579094cf0ed0a8e219
Quentin Guillemin <quentin.guillemin@recia.fr>
2024-06-19 14:20:11 +0200

    build!: from jest to vitest

--- src/test/javascript/spec/services/base64Utils.spec.js
+++ src/test/javascript/spec/services/base64Utils.spec.js
@@ -1,14 +1,15 @@
-import Base64Utils from "@/services/util/Base64Utils.js";
+import Base64Utils from '@/services/util/Base64Utils.js';
+import { describe, expect, it } from 'vitest';
 
 // Tests unitaires sur la classe utilitaire Base64Utils
-describe("Base64Utils.js tests", () => {
-  it("test 1 Base64Utils - encode/decode", () => {
-    var value = "testString";
+describe('Base64Utils.js tests', () => {
+  it('test 1 Base64Utils - encode/decode', () => {
+    let value = 'testString';
     expect(Base64Utils.decode(Base64Utils.encode(value))).toStrictEqual(value);
   });
 
-  it("test 2 Base64Utils - decode/encode", () => {
-    var value = "ZmlsZXMvNDlcMjAyMjAxXDIwMjIwMTIzMTQzNDA2LmpwZw==";
+  it('test 2 Base64Utils - decode/encode', () => {
+    let value = 'ZmlsZXMvNDlcMjAyMjAxXDIwMjIwMTIzMTQzNDA2LmpwZw==';
     expect(Base64Utils.encode(Base64Utils.decode(value))).toStrictEqual(value);
   });
 });
--- src/test/javascript/spec/services/configurationService.spec.js
+++ src/test/javascript/spec/services/configurationService.spec.js
@@ -1,45 +1,42 @@
-import ConfigurationService from "@/services/admin/ConfigurationService.js";
-import FetchWrapper from "@/services/util/FetchWrapper.js";
+import ConfigurationService from '@/services/admin/ConfigurationService.js';
+import FetchWrapper from '@/services/util/FetchWrapper.js';
+import { beforeEach, describe, expect, it, vi } from 'vitest';
 
-jest.mock("@/services/util/FetchWrapper.js");
+vi.mock('@/services/util/FetchWrapper.js');
 
-describe("ConfigurationService.js tests", () => {
+describe('ConfigurationService.js tests', () => {
   beforeEach(() => {
-    jest.clearAllMocks();
+    vi.clearAllMocks();
   });
 
-  it("test 1 ConfigurationService - get", (done) => {
+  it('test 1 ConfigurationService - get', () => {
     const response = {
       data: {
-        key1: "val1",
-        key2: "val2",
+        key1: 'val1',
+        key2: 'val2',
       },
       headers: [],
     };
-    FetchWrapper.getJson = jest.fn().mockReturnValue(Promise.resolve(response));
+    FetchWrapper.getJson = vi.fn().mockReturnValue(Promise.resolve(response));
 
     ConfigurationService.get().then((value) => {
       expect(FetchWrapper.getJson).toHaveBeenCalledTimes(1);
-      expect(FetchWrapper.getJson).toHaveBeenCalledWith(
-        "management/configprops"
-      );
-      expect(value).toStrictEqual(["val1", "val2"]);
-      done();
+      expect(FetchWrapper.getJson).toHaveBeenCalledWith('management/configprops');
+      expect(value).toStrictEqual(['val1', 'val2']);
     });
   });
 
-  it("test 2 ConfigurationService - getEnv", (done) => {
+  it('test 2 ConfigurationService - getEnv', () => {
     const response = {
       data: {},
       headers: [],
     };
-    FetchWrapper.getJson = jest.fn().mockReturnValue(Promise.resolve(response));
+    FetchWrapper.getJson = vi.fn().mockReturnValue(Promise.resolve(response));
 
     ConfigurationService.getEnv().then((value) => {
       expect(FetchWrapper.getJson).toHaveBeenCalledTimes(1);
-      expect(FetchWrapper.getJson).toHaveBeenCalledWith("management/env");
+      expect(FetchWrapper.getJson).toHaveBeenCalledWith('management/env');
       expect(value).toStrictEqual(response);
-      done();
     });
   });
 });
