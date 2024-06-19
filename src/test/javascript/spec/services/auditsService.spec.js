f7cd318d879294b4e40c655393f187e5c095c06d
Quentin Guillemin <quentin.guillemin@recia.fr>
2024-06-19 14:20:11 +0200

    build!: from jest to vitest

--- src/test/javascript/spec/services/auditsService.spec.js
+++ src/test/javascript/spec/services/auditsService.spec.js
@@ -1,44 +1,43 @@
-import AuditsService from "@/services/admin/AuditsService.js";
-import FetchWrapper from "@/services/util/FetchWrapper.js";
+import AuditsService from '@/services/admin/AuditsService.js';
+import FetchWrapper from '@/services/util/FetchWrapper.js';
+import { beforeEach, describe, expect, it, vi } from 'vitest';
 
-jest.mock("@/services/util/FetchWrapper.js");
+vi.mock('@/services/util/FetchWrapper.js');
 
-describe("AuditsService.js tests", () => {
+describe('AuditsService.js tests', () => {
   beforeEach(() => {
-    jest.clearAllMocks();
+    vi.clearAllMocks();
   });
 
-  it("test 1 AuditsService - findAll", (done) => {
+  it('test 1 AuditsService - findAll', () => {
     const response = {
       data: [],
       headers: [],
     };
-    FetchWrapper.getJson = jest.fn().mockReturnValue(Promise.resolve(response));
+    FetchWrapper.getJson = vi.fn().mockReturnValue(Promise.resolve(response));
 
     AuditsService.findAll().then((value) => {
       expect(FetchWrapper.getJson).toHaveBeenCalledTimes(1);
-      expect(FetchWrapper.getJson).toHaveBeenCalledWith("api/audits/all");
+      expect(FetchWrapper.getJson).toHaveBeenCalledWith('api/audits/all');
       expect(value).toStrictEqual(response);
-      done();
     });
   });
 
-  it("test 2 AuditsService - findByDates", (done) => {
+  it('test 2 AuditsService - findByDates', () => {
     const response = {
       data: [],
       headers: [],
     };
-    FetchWrapper.getJson = jest.fn().mockReturnValue(Promise.resolve(response));
+    FetchWrapper.getJson = vi.fn().mockReturnValue(Promise.resolve(response));
 
     const fromDate = new Date(2022, 0, 1, 0, 0, 0);
     const toDate = new Date(2022, 0, 31, 0, 0, 0);
     AuditsService.findByDates(fromDate, toDate).then((value) => {
       expect(FetchWrapper.getJson).toHaveBeenCalledTimes(1);
       expect(FetchWrapper.getJson).toHaveBeenCalledWith(
-        "api/audits/byDates?fromDate=2022-01-01T00%3A00%3A00.000Z&toDate=2022-01-31T00%3A00%3A00.000Z"
+        'api/audits/byDates?fromDate=2022-01-01T00%3A00%3A00.000Z&toDate=2022-01-31T00%3A00%3A00.000Z',
       );
       expect(value).toStrictEqual(response);
-      done();
     });
   });
 });
