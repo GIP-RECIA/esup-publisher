972f01ec798a79eb67ff2cbe824c43970577fdad
Quentin Guillemin <quentin.guillemin@recia.fr>
2024-11-15 11:42:38 +0100

    fix: missing back version when migrate to vite
    Refs: #fd5d6aa195df0f2d9990207c58767f0c114d0b08

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,15 +1,31 @@
 /* eslint-disable node/prefer-global/process */
+import { readFileSync } from 'node:fs'
 import { createRequire } from 'node:module'
 import { fileURLToPath, URL } from 'node:url'
 import ckeditor5 from '@ckeditor/vite-plugin-ckeditor5'
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig, loadEnv } from 'vite'
+import { parseString } from 'xml2js'
 
 const require = createRequire(import.meta.url)
 
 export default ({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
+
+  const backVersion = () => {
+    let version
+    const pomXml = readFileSync('./pom.xml', 'utf8')
+    parseString(pomXml, (err, result) => {
+      if (err)
+        console.error(err)
+      else version = result.project.version[0]
+    })
+
+    return version
+  }
+
+  process.env.VITE_BACK_VERSION = backVersion()
 
   return defineConfig({
     base: `${process.env.VITE_BACK_BASE_URL}ui`,
