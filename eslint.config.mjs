0cd743a17e85b589fa5bf990b9a66756ea068a6b
Julien Gribonvald <julien.gribonvald@recia.fr>
2026-09-22 22:46:02 +0400

    build(deps): update antfu eslint config to 7.7.3

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,9 @@ import antfu from '@antfu/eslint-config'
 
 export default antfu({
   formatters: true,
+  rules: {
+    'e18e/prefer-static-regex': 'off',
+  },
   vue: {
     overrides: {
       'vue/attribute-hyphenation': ['off'],
